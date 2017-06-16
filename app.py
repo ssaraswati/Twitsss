c8fc75c3b5dfe5e470a036a879d887a04648160a
Shyam Saraswati <shyam.saraswati@gmail.com>
2017-06-16 18:01:24 +1000

    Initial scraper

--- app.py
+++ app.py
@@ -13,11 +13,31 @@ def sendtoLogsChannel(message):
 slackLine = "Tweet Harvester Searching"
 # sendtoLogsChannel(slackLine)
 
+def load_json_memory(fileName):
+    with open(fileName) as data_file:
+        return json.load(data_file)
 
-with open('workfile', 'w') as f:
-    tweets = scraper.scrape_city("Melbourne","-37.9,144.5,100km")
-    for item in tweets:
-        f.write("%s\n" % item)
+def melbournetofile():
+    with open('melbourneDev', 'w') as f:
+        tweets = scraper.scrape_city("Melbourne", "-37.9,144.5,100km")
+        for item in tweets:
+            f.write("%s\n" % item)
+
+def australiatofile():
+
+    australiaCities = load_json_memory("AustraliaCities.json")
+
+    for city in australiaCities:
+
+        with open(city['NAME'], 'a') as f:
+            cityGeocode = str(city['LATITUDE']) + "," + str(city['LONGITUDE']) + "," + str(city['RADIUS']) + "km"
+            tweets = scraper.scrape_city(city['NAME'], cityGeocode)
+
+            for item in tweets:
+                f.write("%s\n" % item)
+
+
+australiatofile()
 
 
 
