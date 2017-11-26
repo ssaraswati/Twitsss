1a7036707e2c20da513d39d178ee18370190919e
= <=shyam.saraswati@gmail.com>
2017-11-26 13:51:41 +1100

    Initial commit to rewrite to streaming and s3

--- scraper/example.config.py
+++ scraper/example.config.py
@@ -3,7 +3,12 @@ twitter = dict(consumer_key='',
                access_key='',
                access_secret='')
 
-slack = dict(logsurl='')
-
 aws = dict(access_key='',
            access_secret_key='', bucket='')
+
+#example bounding box for Melbourne, Au
+boundingBox = [144.0, -38.3, 145.7, -37.5]
+
+buffersize = 100
+
+geo_only = True
